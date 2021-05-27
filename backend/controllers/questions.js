5fe40bddc750c6fd2d326d2472b1941edea16003
yash <learnnewthighs@gmail.com>
2021-05-27 16:53:08 +0530

    Created search API

--- backend/controllers/questions.js
+++ backend/controllers/questions.js
@@ -1,5 +1,24 @@
 import PostQuestion from "../models/postQuestion.js";
 
+// * Post new question
+export const createQuestion = async (req, res) => {
+	const post = req.body;
+
+	const newPost = new PostQuestion({
+		...post,
+		creator: req.userId,
+		createdAt: new Date().toISOString(),
+	});
+
+	try {
+		await newPost.save();
+
+		return res.status(201).json(newPost);
+	} catch (error) {
+		return res.status(409).json({ message: error.message });
+	}
+};
+
 // * Question list for main page
 export const getQuestions = async (req, res) => {
 	// extract the parameters from url and store it in variable
@@ -36,21 +55,25 @@ export const getQuestions = async (req, res) => {
 	}
 };
 
-// * Post new question
-export const createQuestion = async (req, res) => {
-	const post = req.body;
-
-	const newPost = new PostQuestion({
-		...post,
-		creator: req.userId,
-		createdAt: new Date().toISOString(),
-	});
-
+// * Search question
+export const searchQuestions = async (req, res) => {
 	try {
-		await newPost.save();
+		const searchItem = req.query.searchItem;
+		const questionList = await PostQuestion.find({
+			title: { $regex: searchItem, $options: "i" }, // ** Uses regex to get fuzzy search functionality and option i make it case insensitive
+		}).sort({ likeCount: -1 });
 
-		return res.status(201).json(newPost);
+		// **TODO Review and try to implement text index
+		// const questionList = await PostQuestion.find({
+		// 	$text: { $search: searchItem, $caseSensitive: true },
+		// }).sort({ score: { $meta: "textScore" } });
+
+		if (questionsList.length === 0) {
+			return res.status(400).json({ message: "No data found!!!" });
+		}
+
+		return res.status(200).json(questionList);
 	} catch (error) {
-		return res.status(409).json({ message: error.message });
+		return res.status(400).json({ message: error.message });
 	}
 };
--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -1,12 +1,17 @@
 import express from "express";
 
 // custom
-import { getQuestions, createQuestion } from "../controllers/questions.js";
+import {
+	getQuestions,
+	createQuestion,
+	searchQuestions,
+} from "../controllers/questions.js";
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/list/:filter/:filterInfo/:sort/:sortInfo", getQuestions); // **TODO Refactor the method to take parameter from url
 router.post("/new", auth, createQuestion);
+router.get("/list/:filter/:filterInfo/:sort/:sortInfo", getQuestions); // **TODO Refactor the method to take parameter from url
+router.get("/search", searchQuestions);
 
 export default router;
