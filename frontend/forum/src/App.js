ba70297a1864a6120fd8ea6c831328ea6f02234e
yash <learnnewthighs@gmail.com>
2021-05-27 21:58:14 +0530

    Added transition effect on category card

--- frontend/forum/src/App.js
+++ frontend/forum/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import CategoryCard from "./components/category/card/Card";
 
 function App() {
-	return <div className="app"></div>;
+	return (
+		<div className="app">
+			<CategoryCard />
+		</div>
+	);
 }
 
 export default App;
