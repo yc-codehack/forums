5c70678c3b26bb29301949e1106cf8c7c2cba0a6
yash <learnnewthighs@gmail.com>
2021-06-03 14:40:14 +0530

    reducer

--- frontend/forum/src/reducers/questions.js
+++ frontend/forum/src/reducers/questions.js
@@ -4,14 +4,14 @@ export default (questions = [], action) => {
 		case "FETCH_RECENT":
 			return action.payload;
 
-		// case "LIKE":
-		// 	return questions.map((question) =>
-		// 		question._id === action.payload._id ? action.payload : question
-		// 	);
-		// case "DISLIKE":
-		// 	return questions.map((question) =>
-		// 		question._id === action.payload._id ? action.payload : question
-		// 	);
+		case "LIKE":
+			return questions.map((question) =>
+				question._id === action.payload._id ? action.payload : question
+			);
+		case "DISLIKE":
+			return questions.map((question) =>
+				question._id === action.payload._id ? action.payload : question
+			);
 
 		default:
 			return questions;
