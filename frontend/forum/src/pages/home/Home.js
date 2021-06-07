d0c738bc1c0d049f30d05bbe9cfb5750dbdadb0b
yash <learnnewthighs@gmail.com>
2021-06-07 11:17:07 +0530

    Save point Home.js changes

--- frontend/forum/src/pages/home/Home.js
+++ frontend/forum/src/pages/home/Home.js
@@ -4,11 +4,31 @@ import React from "react";
 import Navbar from "../../components/navbar/Navbar.js";
 import QuesList from "../../components/ques/QuesList.js";
 
+// materialUI
+import { Button } from "@material-ui/core";
+
 function Home() {
 	return (
 		<div className="home">
-			<Navbar />
-			<QuesList />
+			{/* header */}
+			<div className="home__header">
+				<Navbar />
+			</div>
+			{/* main */}
+			<div className=""></div>
+
+			{/* left side */}
+			<div className="home__leftSide"></div>
+			{/* middle side */}
+			<div className="home__middleSide">
+				<QuesList />
+			</div>
+			{/* right side */}
+			<div className="home__rightSide">
+				<Button className="home__rightSideBtn">
+					Create a Question
+				</Button>
+			</div>
 		</div>
 	);
 }
