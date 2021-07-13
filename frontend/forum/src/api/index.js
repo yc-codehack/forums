52ff455321bea84e15a6e0d4af6140994273bbd5
yash <learnnewthighs@gmail.com>
2021-07-13 17:09:51 +0530

    changes for test server

--- frontend/forum/src/api/index.js
+++ frontend/forum/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+// const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://yc-forum-v1.herokuapp.com" });
 
 // * Adds authorisation token in header of api request
 API.interceptors.request.use((req) => {
